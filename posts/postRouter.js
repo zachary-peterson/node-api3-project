d580995f5fdabf67b11ff204065ce4873eea5599
Zachary <zp.lambda@yahoo.com>
2020-11-03 17:48:09 -0500

    ...

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -18,11 +18,35 @@ router.delete('/:id', (req, res) => {
 router.put('/:id', (req, res) => {
   // do your magic!
   const { id } = req.params;
-  const change = req.body;
-  
+  const changes = req.body;
+
+  if(isValidUpdate(changes)){
+  db.update(id, changes)
+  .then(count => {
+    if(count === 1) {
+      res.status(200).json({ res: "Updated succesfully."});
+    } else {
+      res.status(404).json({ err: "Record could not be found."});
+    }
+  })
+  .catch(errs => {
+    console.log(errs);
+    res.status(500).json({errorMsg: "Error updating db"});
+  });
+}else {
+  res.status(404).json({errorMsg: "Please update title or contents."});
+}
 });
 
 // custom middleware
+
+function isValid(post) {
+  
+}
+
+function isValidUpdate(changes) {
+  return Boolean(changes.title || changes.contents);
+}
 
 function validatePostId(req, res, next) {
   // do your magic!
--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 
 const server = express();
+server.use(express.json());
+
+server.use(function(req, res) {
+  res.status(404).send("Ain't nobody got time for that!");
+});
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +13,10 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log('...');
 
+  next();
+}
+server.use(logger);
 module.exports = server;
